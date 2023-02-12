0614997d75b56b3004d21acde2f5a61f565ec2a9
Camille Linan <camille.linan@hotmail.com>
2023-02-12 20:49:35 +0100

    init project + animation widget

---
+++ android/app/src/main/kotlin/com/example/appyoga/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appyoga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+const dRed = Color(0xFFE9717D);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Yoga App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
